96784f7091427194759b326cf0380ac195557cc3
Aditya Kumar <muskianand89@gmail.com>
2026-02-10 15:34:26 +0530

    updated 2

--- src/app/_home/Hero.tsx
+++ src/app/_home/Hero.tsx
@@ -1,3 +1,5 @@
+import { PiCaretDoubleDown } from "react-icons/pi";
+
 export default function Hero() {
   return (
     <section className="bg-primary-foreground relative flex h-screen items-center overflow-hidden">
@@ -10,22 +12,20 @@ export default function Hero() {
       </div>
       <div className="relative z-10 mx-auto w-full max-w-7xl px-8">
         <div className="max-w-4xl">
-          <span className="text-primary bg-primary-foreground px-3 py-2 text-xs font-bold tracking-[0.4em] uppercase">
+          <span className="bg-primary-foreground text-primary px-3 py-2 text-xs font-bold tracking-[0.4em] uppercase">
             Residential &amp; Wellness Studio
           </span>
+
           <h1 className="mt-8 mb-12 font-serif text-6xl leading-[1.1] font-light text-white md:text-8xl">
-            {/* Architecture for living, wellbeing, and{" "}
-            <span className="italic">long-term calm.</span> */}
             Architecture Design guided by{" "}
             <span className="font-medium italic">Human Biology</span>, not
             trends.
           </h1>
         </div>
       </div>
-      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce text-white/30">
-        <span className="material-symbols-outlined text-3xl">
-          keyboard_double_arrow_down
-        </span>
+
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce">
+        <PiCaretDoubleDown className="text-3xl text-white/30" />
       </div>
     </section>
   );
