aacd34d6638bafd87ce84bf8828d1bb3655371ff
Aditya Kumar <muskianand89@gmail.com>
2026-02-13 13:12:43 +0530

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import logo from "../../public/logo.png";
+
 export default function Loader() {
   return (
     <div className="bg-sand-50 flex h-screen w-full items-center justify-center">
-      <div className="font-serif text-4xl tracking-wide md:text-6xl">
-        Roshni Studio
-      </div>
+      <Image
+        src={logo}
+        alt="Roshni Studio Logo"
+        priority
+        className="w-40 md:w-60"
+      />
     </div>
   );
 }
